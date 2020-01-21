c012f768da7bbb394b8a49b3010d7084fccfefbe
Rodrigo Felix <rodrigo.felix321@gmail.com>
2020-01-20 21:49:15 -0300

    Método de importação de xml

--- src/main/java/br/com/ciceroednilson/usuario/controller/CadastrarPessoaController.java
+++ src/main/java/br/com/ciceroednilson/usuario/controller/CadastrarPessoaController.java
@@ -1,8 +1,20 @@
 package br.com.ciceroednilson.usuario.controller;
 
+import java.io.IOException;
+
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.primefaces.model.UploadedFile;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
 
 import br.com.ciceroednilson.model.PessoaModel;
 import br.com.ciceroednilson.repository.PessoaRepository;
@@ -20,6 +32,16 @@ public class CadastrarPessoaController {
 	
 	@Inject
 	PessoaRepository pessoaRepository;
+	
+	private UploadedFile file;
+	
+	public UploadedFile getFile() {
+		return file;
+	}
+
+	public void setFile(UploadedFile file) {
+		this.file = file;
+	}
 
 	public PessoaModel getPessoaModel() {
 		return pessoaModel;
@@ -45,8 +67,69 @@ public class CadastrarPessoaController {
 		
 		Uteis.MensagemInfo("Registro cadastrado com sucesso!");
 		
-		
-		
 	}
 	
+	/*
+	 * REALIZANDO UPLOAD DE ARQUIVO
+	 */
+	public void UploadRegistros() {
+		
+		DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+		
+		try {
+			
+			if(this.file.getFileName().equals("")) {
+				Uteis.MensagemAtencao("Nenhum arquivo selecionado");
+			}
+			
+			DocumentBuilder builder = factory.newDocumentBuilder();
+			
+			Document doc = builder.parse(this.file.getInputstream());
+			
+			Element element = doc.getDocumentElement();
+			
+			NodeList nodes = element.getChildNodes();
+			
+			for (int i = 0; i < nodes.getLength(); i++) {
+				
+				Node node = nodes.item(i);
+				
+				if(node.getNodeType() == Node.ELEMENT_NODE) {
+					
+					Element elementPessoa = (Element) node;
+					
+					//PEGANDO OS VALORES DO ARQUIVO XML
+					String nome = elementPessoa.getElementsByTagName("nome").item(0).getChildNodes().item(0).getNodeValue();
+					String sexo = elementPessoa.getElementsByTagName("sexo").item(0).getChildNodes().item(0).getNodeValue();
+					String email = elementPessoa.getElementsByTagName("email").item(0).getChildNodes().item(0).getNodeValue();
+					String endereco = elementPessoa.getElementsByTagName("endereco").item(0).getChildNodes().item(0).getNodeValue();
+					
+					PessoaModel newPessoaModel = new PessoaModel();
+					
+					newPessoaModel.setUsuarioModel(this.usuarioController.GetUsuarioSession());
+					newPessoaModel.setEmail(email);
+					newPessoaModel.setEndereco(endereco);
+					newPessoaModel.setNome(nome);
+					newPessoaModel.setOrigemCadastro("X");
+					newPessoaModel.setSexo(sexo);
+					
+					//SALVANDO UM REGISTRO QUE VEIO DO ARQUVIO XML
+					pessoaRepository.SalvarNovoRegistro(newPessoaModel);
+				}
+			}
+			
+			Uteis.MensagemInfo("Registros cadastrados com sucesso!");
+			
+		} catch (ParserConfigurationException e) {
+			e.printStackTrace();
+			
+		} catch (SAXException e) {
+			e.printStackTrace();
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+	}
+
 }
